da9385225bb57a336d5b7a38fd78926cb1344911
Aknazarova <aknazarova_a@bi.group>
2020-03-21 22:56:15 +0600

    f

--- net/n.go
+++ net/n.go
@@ -11,6 +11,10 @@ import (
 	"time"
 )
 
+type Data struct {
+	User string
+}
+
 type client chan<- string // Канал исходящих сообщений
 
 var (
@@ -25,7 +29,6 @@ func broadcaster() {
 	for {
 		select {
 		case msg := <-messages:
-			// fmt.Println("received", msg)
 			for cli := range clients {
 				cli <- msg
 			}
@@ -35,10 +38,8 @@ func broadcaster() {
 		case cli := <-leaving:
 			delete(clients, cli)
 			close(cli)
-
 		}
 	}
-
 }
 
 func handleConn(conn net.Conn) {
@@ -48,12 +49,12 @@ func handleConn(conn net.Conn) {
 		os.Exit(1)
 	}
 	ch := make(chan string) //Исходящие сообщения клиентов
-	user := ""
 	go clientWriter(conn, ch)
 
 	io.WriteString(conn, string(img))
 	io.WriteString(conn, "\r\n[ENTER YOUR NAME]:")
 
+	user := ""
 	client := []string{}
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
@@ -69,84 +70,49 @@ func handleConn(conn net.Conn) {
 	}
 	if user == "" {
 		return
-	} else {
-		fmt.Fprintf(conn, "[%s][%s]: ", time.Now().Format("2006-01-02 15:04:05"), user)
 	}
-	apuser := []string{}
-	newuser := ""
-	for scanner.Scan() {
-		newuser = scanner.Text()
-		if user == newuser {
-			fmt.Fprintf(conn, "%s", "Данное имя уже используется")
-		}
+	fmt.Fprintf(conn, "[%s][%s]: ", time.Now().Format("2006-01-02 15:04:05"), user)
+	// in := bufio.NewScanner(conn)
+	// user := bufio.NewScanner(conn)
 
-		apuser = append(apuser, newuser)
-		break
-	}
+	messages <- "\n" + user + " has joined our chat..."
 
-	enter := "\n" + user + " has joined our chat..." + "\n"
-
-	select {
-	case messages <- enter:
-		// messages<-  "\n" + "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": "
-		// messages <- enter
-		messages <- "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": "
-
-	}
 	entering <- ch
-
-	// output := ""
-
+	// input := bufio.NewReader(conn)
+	// user, _ := input.ReadString('\n')
 	for scanner.Scan() {
 		letter := scanner.Text()
+		if err != nil {
+			break
+		}
+		output := ""
+		broadcastMessage := fmt.Sprintf("[%s][%s]: %s\n", time.Now().Format("2006-01-02 15:04:05"), user, letter)
+		nameHandler := fmt.Sprintf("[%s][%s]: ", time.Now().Format("2006-01-02 15:04:05"), user)
+		// Message := fmt.Sprintf("[%s][%s]: %s\n", time.Now().Format("2006-01-02 15:04:05"), letter)
+		// fmt.Println(nameHandler)
+		for _, w := range client {
+			if w != user {
 
-		// broadcastMessage := fmt.Sprintf("[%s][%s]: %s\n", time.Now().Format("2006-01-02 15:04:05"), user, letter)
-		// nameHandler := fmt.Sprintf("[%s][%s]: ", time.Now().Format("2006-01-02 15:04:05"), user)
-		output := "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": " 
+				output = broadcastMessage + nameHandler + letter //правильный вариант [2020-03-13 21:23:10][Aika]: 7
+			} else {
 
-		for _, c := range client {
-			for _, v := range apuser {
-				if c != v {
-					fmt.Fprintf(conn, output)
-				}else{
-					fmt.Fprintf(conn, "%s", "Данное имя уже используется")
-				}
+				output = broadcastMessage + letter
 			}
-			// fmt.Println(client)
-			// if c == user {
-
-			// 	fmt.Fprintf(conn, nameHandler)
-			// 	// fmt.Fprintf(conn, broadcastMessage,nameHandler,letter)
-			// } else {
-			// 	// fmt.Fprintf(conn, nameHandler)
-			// 	messages <-" выходи уже f"
-			// return
-			// messages <- new
 
 		}
-
+		messages <- output
 		// output = "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": " + letter
 
-		// output = letter
-
-		messages <- letter
-
-		new := "\n" + "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": "
-		messages <- new
-
-		// messages <- str
-		// output = "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": " + letter
 	}
 
 	messages <- "\n" + user + " has left our chat"
-	messages <- "\n" + "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + "[" + user + "]" + ": "
 	leaving <- ch
 	conn.Close()
 }
 
 func clientWriter(conn net.Conn, ch <-chan string) {
 	for msg := range ch {
-		fmt.Fprintf(conn, msg)
+		fmt.Fprintln(conn, msg)
 	}
 }
 
